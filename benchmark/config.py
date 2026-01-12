d37f9fd7612dbaeb2ff5ec4c3a70fe961b8241bb
AliasJeff <2022248374@qq.com>
2026-01-12 23:20:23 +0800

    220

--- benchmark/config.py
+++ benchmark/config.py
@@ -20,7 +20,7 @@ class Config:
     TORCH_DTYPE = torch.float16
     LOAD_IN_4BIT = True
 
-    BATCH_SIZE = 80
+    BATCH_SIZE = 220
     MAX_NEW_TOKENS = 64
     TEMPERATURE = 0.7
     TOP_P = 0.9
